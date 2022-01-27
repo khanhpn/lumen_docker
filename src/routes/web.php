6ab502f67a5527c9708c77294860abe168b4b8a9
khanhpn <phamkhanh90@gmail.com>
2022-01-27 18:12:12 +0700

    login page

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Http\Request;
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
@@ -12,6 +13,14 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+$router->get('/test', function() use ($router) {
+    return view('test');
+});
+
+$router->post('/test1', function(Request $request) use ($router) {
+    print($request->name);
+    return view('test', ['quote' => 10]);
+});
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
